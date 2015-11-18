76ca17edf73ff958fcf618a34cacec3513065047
Gavin Staniforth <gsdevme@users.noreply.github.com>
2015-11-18 17:18:11 +0000

    Updated version

--- metadata.rb
+++ metadata.rb
@@ -1,4 +1,5 @@
 name             'process-stalker'
 description      'Installs process-stalker'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.1.2'
+depends          'line'
